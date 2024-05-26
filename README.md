c588a12ff5f2c369af17336b989e8148a94471c2
Kilian Melcher <kilian.melcher@vtex.com>
2024-05-26 12:52:32 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+## Dependencies
+
+- Install [Python3](https://www.python.org/downloads/)
+
+- Install Selenium
+
+  ```bash
+  pip3 install selenium
+  ```
+
+- Install Google Chrome
+
+- Download [Chrome for testing](https://googlechromelabs.github.io/chrome-for-testing/#stable)
+
+> [!NOTE]
+> Check your chrome version on Settings >> About Chrome
+
+![alt text](image.png)
+
+![alt text](image-1.png)
+
+## Execute
+
+Run the script in the CLI. It will loop continuously every 10min.
+
+```py
+python3 scrape.py
+```
---
+++ scrape.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+
+BUERGERBUERO_WEBSITE = 'https://tempus-termine.com/termine/index.php?anlagennr=13&anlagentoken=Uq9dpZv7&anwendung=1&infotext=buergerbuero'
+TIME_INTERVAL = 10 * 60 # 10 min
+
+ANMELDUNG = 'Anmeldung-422-mittermin'
+REISEPASS = 'Antrag_RP-416-mittermin'
+CHOOSE_DATETIME = '//input[@value="Datum und Uhrzeit auswählen"]'
+CHECKBOX = '//input[@type="checkbox"]'
+CONFIRM = '//input[@id="confirm"]'
+TERMIN_DATE = 'nav_menu2'
+
+def select(driver, id, value):
+  select_tag= driver.find_element(By.ID, id)
+  selector = Select(select_tag)
+  selector.select_by_value(value)
+
+def click_button(driver, xpath):
+  button = driver.find_element(By.XPATH, xpath)
+  button.click()
+
+def click_buttons(driver, xpath):
+  buttons = driver.find_elements(By.XPATH, xpath)
+  for button in buttons:
+    button.click()
+
+def find_text(driver, classname):
+  return driver.find_element('class name', classname).text
+
+def navigate(driver):
+  select(driver, ANMELDUNG, "1")
+  select(driver, REISEPASS, "1")
+
+  click_button(driver, CHOOSE_DATETIME)
+  click_buttons(driver, CHECKBOX)
+  click_button(driver, CONFIRM)
+
+  earliest_date = find_text(driver, TERMIN_DATE)
+
+  print (earliest_date)
+  
+
+while True:
+  driver = webdriver.Chrome()
+  driver.get(BUERGERBUERO_WEBSITE)
+  
+  navigate(driver)
+  
+  driver.quit()
+
+  time.sleep(TIME_INTERVAL)
