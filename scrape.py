c2f1ae629fb113cee498bfa439952d6f2b048c5c
Elmar Melcher <elmar@dsc.ufcg.edu.br>
2024-05-27 19:42:29 +0200

    dirty modification to make some noise if the month is good

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from playsound import playsound
 import time
 
 BUERGERBUERO_WEBSITE = 'https://tempus-termine.com/termine/index.php?anlagennr=13&anlagentoken=Uq9dpZv7&anwendung=1&infotext=buergerbuero'
@@ -41,6 +42,9 @@ def navigate(driver):
   click_button(driver, CONFIRM)
 
   earliest_date = find_text(driver, TERMIN_DATE)
+  day_month = earliest_date.split()[1].split(".")
+  if int(day_month[1]) < 7:
+    playsound('/opt/libreoffice7.6/share/gallery/sounds/gong.wav')
 
   print (earliest_date)
   
